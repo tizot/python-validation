eb62a2a37384933b366a41c4c067f11c090179e5
Camille Masset <masset.camille@gmail.com>
2020-10-11 02:15:30 +0200

    Refactor with Constraint and Typed

--- main.py
+++ main.py
@@ -1,39 +1,52 @@
-class Integer:
+class Constraint:
+    @classmethod
+    def check(cls, val): ...
+
+
+class Typed(Constraint):
+    type = None
+
     @classmethod
     def check(cls, val):
-        assert isinstance(val, int), "Expected int"
+        assert isinstance(val, cls.type), f"Expected {cls.type}"
+        super().check(val)
 
 
-class Float:
-    @classmethod
-    def check(cls, val):
-        assert isinstance(val, float), "Expected float"
+class Integer(Typed):
+    type = int
 
 
-class String:
-    @classmethod
-    def check(cls, val):
-        assert isinstance(val, str), "Expected str"
+class Float(Typed):
+    type = float
 
 
-class Positive:
+class String(Typed):
+    type = str
+
+
+class Positive(Constraint):
     @classmethod
     def check(cls, val):
         assert val > 0, "Expected > 0"
+        super().check(val)
 
 
-class Nonempty:
+class Nonempty(Constraint):
     @classmethod
     def check(cls, val):
         assert len(val) > 0, "Expected non empty"
+        super().check(val)
+
+
+class PositiveInteger(Integer, Positive): ...
+class PositiveFloat(Float, Positive): ...
+class NonemptyString(String, Nonempty): ...
 
 
 def gcd(a: int, b: int):
     """Computes the greatest common divisor of a and b"""
-    Integer.check(a)
-    Positive.check(a)
-    Integer.check(b)
-    Positive.check(b)
+    PositiveInteger.check(a)
+    PositiveInteger.check(b)
     if a > b:
         return gcd(b, a)
     while b != 0:
@@ -42,10 +55,8 @@ def gcd(a: int, b: int):
 
 
 def format(brand: str, price: float):
-    String.check(brand)
-    Nonempty.check(brand)
-    Float.check(price)
-    Positive.check(price)
+    NonemptyString.check(brand)
+    PositiveFloat.check(price)
     return f"{brand}: ${price:.2f}"
 
 
